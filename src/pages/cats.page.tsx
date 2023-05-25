5ad71e8fc2544e983b4bf93057a369ca15e8c562
premma <premma1300@gmail.com>
2023-05-26 02:08:12 +0700

    useInfiniteQuery

--- src/pages/cats.page.tsx
+++ src/pages/cats.page.tsx
@@ -1,16 +1,41 @@
 import React, { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useInfiniteQuery } from '@tanstack/react-query';
 
-import { catApi, userApi } from 'src/api';
+import { catApi } from 'src/api';
 
 const Cats = () => {
 	const [paginate, setPaginate] = useState({ page: 1, limit: 1 });
 
-	const { data, isLoading, isError, error, fetchStatus, isFetching, refetch } = useQuery({
+	// const { data, isLoading, isError, error, fetchStatus, isFetching, refetch } = useQuery({
+	// 	queryKey: ['cats', paginate.page, paginate.limit],
+	// 	queryFn: () => catApi.allCats(paginate.page, paginate.limit),
+	// 	staleTime: 10000,
+	// 	retry: false,
+	// });
+
+	const {
+		data,
+		isLoading,
+		isError,
+		error,
+		fetchStatus,
+		isFetching,
+		refetch,
+		hasNextPage,
+		fetchNextPage,
+		isFetchingNextPage,
+	} = useInfiniteQuery({
 		queryKey: ['cats', paginate.page, paginate.limit],
-		queryFn: () => catApi.allCats(paginate.page, paginate.limit),
+		queryFn: ({ pageParam = 1 }) => catApi.allCats(pageParam, paginate.limit),
 		staleTime: 10000,
-		retry: false,
+		getNextPageParam: (lastPage, pages) => {
+			console.log('pages :>> ', pages, lastPage.numberOfPage);
+			if (pages.length < lastPage.numberOfPage) {
+				return pages.length + 1;
+			} else {
+				return undefined;
+			}
+		},
 	});
 
 	const handleNextPage = () => {
@@ -32,6 +57,7 @@ const Cats = () => {
 		return <>{error?.message}</>;
 	}
 
+	console.log('hasNextPage :>> ', hasNextPage);
 	return (
 		<div>
 			fetch status: {fetchStatus}
@@ -42,23 +68,40 @@ const Cats = () => {
 			<br />
 			<br />
 			<h3>Cats</h3>
-			{data?.cats.map(cat => {
+			{/* {data?.cats.map(cat => {
 				return (
 					<React.Fragment key={cat._id}>
 						<p>{cat.name}</p>
 					</React.Fragment>
 				);
+			})} */}
+			{data.pages.map((group, index) => {
+				return (
+					<React.Fragment key={index}>
+						{group.cats.map(cat => {
+							return (
+								<React.Fragment key={cat._id}>
+									<p> {cat.name}</p>
+								</React.Fragment>
+							);
+						})}
+					</React.Fragment>
+				);
 			})}
+			{isFetchingNextPage ? <p>loading...</p> : null}
 			<br />
 			<br />
 			<br />
-			<button onClick={handlePrevPage} disabled={paginate.page === 1}>
+			<button disabled={!hasNextPage} onClick={() => fetchNextPage()}>
+				load more
+			</button>
+			{/* <button onClick={handlePrevPage} disabled={paginate.page === 1}>
 				prev
 			</button>
 			{paginate.page}
 			<button onClick={handleNextPage} disabled={paginate.page === data.numberOfPage}>
 				next
-			</button>
+			</button> */}
 		</div>
 	);
 };
