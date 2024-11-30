0515c8a66318be8f9917b065a7e012b41dfb081f
premma <premma1300@gmail.com>
2024-11-30 13:01:07 +0700

    Comment out code in test page

--- src/pages/test-page.tsx
+++ src/pages/test-page.tsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+// import { useQuery } from '@tanstack/react-query';
 
-import { userApi } from 'src/api';
+// import { userApi } from 'src/api';
 
 const TestPage = () => {
   const [inputText, setInputText] = useState('');
   const [displayText, setDisplayText] = useState('');
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['test', 'user'],
-    queryFn: userApi.users,
-    staleTime: 10000,
-  });
+  // const { data, isLoading, isError } = useQuery({
+  //   queryKey: ['test', 'user'],
+  //   queryFn: userApi.users,
+  //   staleTime: 10000,
+  // });
 
   return (
     <>
@@ -31,16 +31,16 @@ const TestPage = () => {
         set text
       </button>
       <p>{displayText}</p>
-      {isError && <p>Error</p>}
-      {isLoading && <p>Loading...</p>}
+      {/* {isError && <p>Error</p>} */}
+      {/* {isLoading && <p>Loading...</p>} */}
       <div data-testid='container'>
-        {data?.map(user => {
+        {/* {data?.map(user => {
           return (
             <p key={user.id} data-testid='user-card'>
               {user.name}
             </p>
           );
-        })}
+        })} */}
       </div>
     </>
   );
